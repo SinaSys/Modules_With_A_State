aa8f01acc8220da6c4585c9e1811a1f90c9fc950
SinaSystem <sinasystem47@gmail.com>
2020-06-05 00:43:06 +0430

    Initial commit

---
+++ app/src/main/java/com/androidtutz/anushka/didemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.androidtutz.anushka.didemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import javax.inject.Inject;
+
+public class MainActivity extends AppCompatActivity {
+    @Inject
+    SmartPhone smartPhone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //SmartPhoneComponent smartPhoneComponent = DaggerSmartPhoneComponent.create();
+        //smartPhone = smartPhoneComponent.getSmartPhone();
+        SmartPhoneComponent smartPhoneComponent = DaggerSmartPhoneComponent.builder()
+                                  .memoryCardModule(new MemoryCardModule(100))
+                                  .build();
+        smartPhoneComponent.inject(this);
+
+        smartPhone.makeACall();
+
+    }
+}
---
+++ app/src/main/java/com/androidtutz/anushka/didemo/SmartPhoneComponent.java
@@ -0,0 +1,11 @@
+package com.androidtutz.anushka.didemo;
+
+import dagger.Component;
+
+@Component(modules = {MemoryCardModule.class,NCBatteryModule.class})
+public interface SmartPhoneComponent {
+
+   // SmartPhone getSmartPhone();
+    void inject(MainActivity mainActivity);
+
+}
